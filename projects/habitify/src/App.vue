5828bfdea860284971ce4e4c7d8ea3009aa19dc8
j3wlius <tamalejulius5@gmail.com>
2025-12-19 19:39:01 +0300

    Update habitify app

--- projects/habitify/src/App.vue
+++ projects/habitify/src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const value = ref("");
+</script>
 
 <template>
-  <h1 class="bg-red-500">Welcome to Habitify</h1>
+  <div class="py-16 flex justify-center">
+    <div class="max-h-[500] w-2/3 bg-white">
+      <h1>Welcome to Habitify</h1>
+      <input v-model="value" placeholder="Input tast" />
+    </div>
+  </div>
 </template>
-
-<style scoped></style>
